46b5994f960c3f5fea3a1266ef42cf4f88663902
Will Solow <soloww@oregonstate.edu>
2026-02-16 16:08:55 -0800

    updated hierarchical RL policy

--- kinova_tasks/isaac_tasks/direct/kinova_reach/kinova_reach_env.py
+++ kinova_tasks/isaac_tasks/direct/kinova_reach/kinova_reach_env.py
@@ -35,7 +35,7 @@ class KinovaReachEnvCfg(SkillsDirectRLEnvCfg):
 
     joint_ids = [0, 1, 2, 3, 4, 5, 6, 7]
 
-    skills = ["reach_xyz", "orient_y"]
+    skills = ["reach_xyz"]
 
     # simulation
     sim: SimulationCfg = SimulationCfg(
